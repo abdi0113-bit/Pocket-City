57f2c010acb20d5f18291a72fd27e26b0c9f51cb
Mirondotpy <miron.v.attard@gmail.com>
2025-12-21 15:45:49 -0500

    Created Building object structure

--- Game Code/Buildings.py
+++ Game Code/Buildings.py
@@ -1,5 +1,18 @@
 import pygame
 
 class Building():
-    def __init__(self, name):
+    def __init__(self, name, cost):
         self.name = name
+        self.cost = cost
+
+    def whenActivated(self):
+        if self.name == 'Brick House':
+            pass
+        elif self.name == 'Log House':
+            pass
+
+    def whenPlaced(self):
+        if self.name == 'Brick House':
+            pass
+        elif self.name == 'Log House':
+            pass

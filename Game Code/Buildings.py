d2c92bc160571046c939c33e689cec64ffb2af63
Mirondotpy <miron.v.attard@gmail.com>
2026-01-13 13:21:46 -0500

    Buffed hospital

--- Game Code/Buildings.py
+++ Game Code/Buildings.py
@@ -221,7 +221,7 @@ class Building():
             chargeIncrease += 1000000
 
         elif self.name == 'Hospital':
-            if random.random() < 0.1: # 10% chance
+            if random.random() < 0.2: # 20% chance
                 livesIncrease = 1
 
         return newMultipliers, newAddends, newCoinMultipliers, chargeIncrease, livesIncrease
@@ -334,7 +334,7 @@ uncommonBuildings = {'Condo' : Building('Condo', 3, 2,  'Condo', 'Uncommon', (10
 rareBuildings = {'Power Plant': Building('Power Plant', 5, 4,  'Power Plant', 'Rare', (30,0), (0,0), 'Power Plant\n--------------\n+ 30 Score\nwhen activated.\n+ 50 Charge before round'),
                 'Mansion' : Building('Mansion', 5, 4,  'Mansion', 'Rare', (100,0), (0,0), 'Mansion\n--------------\n+ 100 Score\nwhen activated.\n- 20 Score for each\nnearby building\nbefore round.'),
                 'Church' : Building('Church', 4, 3, 'Church', 'Rare', (20,0), (0,0), 'Church\n--------------\n+ 20 Score\nwhen activated.\nMultiply by 1.1 per\nnearby Church.\nMultiply by 2.5 per\nnearby Giant Statue.'),
-                'Hospital' : Building('Hospital', 5, 4, 'Hospital', 'Rare', (20,0), (0,0), 'Hospital\n--------------\n+ 20 Score\nwhen activated.\nHas a 10% chance\nof restoring 1\nlife before round.'),
+                'Hospital' : Building('Hospital', 5, 4, 'Hospital', 'Rare', (20,0), (0,0), 'Hospital\n--------------\n+ 20 Score\nwhen activated.\nHas a 20% chance\nof restoring 1\nlife before round.'),
                 'Fire Station' : Building('Fire Station', 5, 4, 'Fire Station', 'Rare', (20,0), (0,0), 'Fire Station\n--------------\n+ 20 Score\nwhen activated.'),
                 'Ferris Wheel' : Building('Ferris Wheel', 4, 3, 'Ferris Wheel', 'Rare', (15,0), (0,0), 'Ferris Wheel\n--------------\n+ 20 Score\nwhen activated\nMultiplies nearby Businesses\nby 1.5 before round.')}
 
